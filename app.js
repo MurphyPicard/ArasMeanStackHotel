db67f0d4f312824f53c153d5585a7aabc6c93c2b
Ara <ayaghsizian@gmail.com>
2017-09-17 16:32:11 -0400

    sending json 200

--- app.js
+++ app.js
@@ -3,6 +3,16 @@ var app = express();
 
 app.set('port', 2222);
 
+app.get('/', function(req,res){
+  console.log("get the homepage");
+  res.status(404).send('hi');
+});
+
+app.get('/json', function(req,res){
+  console.log("get the json");
+  res.status(200).json({'data':true});
+});
+
 var server = app.listen(app.get('port'), function(){
   var port = server.address().port;
   console.log('Listening on ... ' + port);
