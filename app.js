e9d0f4daf386035f281c62f52f0fea502aadc1ca
Ara <ayaghsizian@gmail.com>
2017-09-17 12:46:44 -0400

    refactored app.js

--- app.js
+++ app.js
@@ -3,5 +3,6 @@ var app = express();
 
 app.set('port', 2222);
 
-app.listen(app.get('port'));
-console.log('Listening on ... ' + app.get('port'));
+app.listen(app.get('port'), function(){
+  console.log('Listening on ... ' + app.get('port'));
+});
