8dcbd3131461efe807af44fbc003b0be21165bef
Ara <ayaghsizian@gmail.com>
2017-09-17 16:37:50 -0400

    sendFile working ok

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 
 app.set('port', 2222);
 
 app.get('/', function(req,res){
   console.log("get the homepage");
-  res.status(404).send('hi');
+  res.status(200).send('hi home');
 });
 
 app.get('/json', function(req,res){
   console.log("get the json");
-  res.status(200).json({'data':true});
+  res.status(200).json({'jsonData':true});
+});
+
+app.get('/file', function(req,res){
+  console.log("get the file");
+  res
+    .status(200)
+    .sendFile(path.join(__dirname, 'app.js'));
 });
 
 var server = app.listen(app.get('port'), function(){
