a397a4d0f89e4396ba12fc37a4c326aa19e8c47a
Ara <ayaghsizian@gmail.com>
2017-09-17 22:12:58 -0400

    fixed the query data off and cnt to calculate returnData correctly

--- api/controllers/hotels.controllers.js
+++ api/controllers/hotels.controllers.js
@@ -1,15 +1,24 @@
 var hotelData = require('../data/hotel-data.json');
 
 module.exports.hotelsGetAll = function(req,res){
-  console.log("get hotel-data.json success?");
+  console.log("getting all hotels");
+  console.log(req.query);
+
+  var off = 0, cnt = 3;
+
+  (req.query && req.query.off) ? off = parseInt(req.query.off, 10) : off = 0 ;
+  (req.query && req.query.cnt) ? cnt = parseInt(req.query.cnt, 10) : cnt = 5 ;
+
+  var returnData = hotelData.slice(off, off+cnt);
+
   res
     .status(200)
-    .json(hotelData);
+    .json(returnData);
 };
 
 module.exports.hotelsGetOne = function(req,res){
   var hotelId = req.params.hotelId; // I named this route in index.js
-  console.log("get one hotel-data.json success?");
+  console.log("getting one hotel with ID of ", hotelId );
   res
     .status(200)
     .json(hotelData[hotelId]); // using the params to pick a specific hotel
