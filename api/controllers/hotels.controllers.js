8df3a2a198eee8a0ebb2d11cd3fdcf4331d3a4e4
Ara <ayaghsizian@gmail.com>
2017-09-17 21:24:54 -0400

    very happy to say params.hotelId is working perfect

--- api/controllers/hotels.controllers.js
+++ api/controllers/hotels.controllers.js
@@ -6,3 +6,11 @@ module.exports.hotelsGetAll = function(req,res){
     .status(200)
     .json(hotelData);
 };
+
+module.exports.hotelsGetOne = function(req,res){
+  var hotelId = req.params.hotelId; // I named this route in index.js
+  console.log("get one hotel-data.json success?");
+  res
+    .status(200)
+    .json(hotelData[hotelId]); // using the params to pick a specific hotel
+};
