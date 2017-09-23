3e4f501a96d2c18146eeeab5989211a447c9eeb7
Ara <ayaghsizian@gmail.com>
2017-09-23 19:36:21 -0400

    posting new hotels but not posting correctly, YET

--- api/controllers/hotels.controllers.js
+++ api/controllers/hotels.controllers.js
@@ -26,6 +26,7 @@ module.exports.hotelsGetAll = function(req,res){
 module.exports.hotelsGetOne = function(req,res){
   var db = dbconn.get();
   var collection = db.collection('hotelCollection');
+
   var hotelId = req.params.hotelId; // I named this route in index.js
   console.log("getting one hotel with ID of ", hotelId );
   collection
@@ -40,9 +41,25 @@ module.exports.hotelsGetOne = function(req,res){
 module.exports.hotelsAddOne = function(req,res){
   var db = dbconn.get();
   var collection = db.collection('hotelCollection');
+  var newHotel;
   console.log('posting new hotel');
-  console.log(req.body);
-  res
-    .status(200)
-    .json(req.body);
+
+  if(req.body && req.body.name && req.body.stars){
+    newHotel = req.body;
+    newHotel.stars = parseInt(req.body.stars, 10);
+
+    console.log("this is new hotel line 51ish: ", newHotel );
+    collection.insertOne(newHotel, function(err, response){
+      console.log(response);
+      res
+        .status(201)
+        .json(response);
+    });
+  }//if
+  else{
+    console.log("Data is missing from body");
+    res.status(400)
+       .json( {message: "required data is missing from body"});
+  }
+
 };
