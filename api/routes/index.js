e372627f61951742f6aac0b1797e99ea99255212
Ara <ayaghsizian@gmail.com>
2017-09-23 20:24:21 -0400

    made a plan for routes, restful api for hotels and reviews

--- api/routes/index.js
+++ api/routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 var ctrlHotels = require('../controllers/hotels.controllers.js');
 
 
-//
+
 router
   .route('/hotels')
   .get(ctrlHotels.hotelsGetAll);
@@ -16,7 +16,37 @@ router
 router
   .route('/hotels/new')
   .post(ctrlHotels.hotelsAddOne);
+/*
 
+
+
+update a specific hotel
+PUT
+/api/hotels/234
+
+delete a specific hotel
+DELETE
+/api/hotels/234
+
+
+
+GET - get all reviews for a specific hotel
+/api/hotels/23/reviews
+
+POST - add a review
+/api/hotels/23/reviews
+
+GET - see one review
+/api/hotels/23/reviews/324234
+
+PUT - update one review
+/api/hotels/23/reviews/242342
+
+DELETE one review
+/api/hotels/23/reviews/123
+
+
+*/
 
 
 
