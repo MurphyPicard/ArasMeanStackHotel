3bf829c3f2feab9f92812a25254346e0380d3b7a
Ara <ayaghsizian@gmail.com>
2017-09-25 10:15:05 -0400

    need to test updateReview and deleteHotel functions

--- api/controllers/reviews.controllers.js
+++ api/controllers/reviews.controllers.js
@@ -102,7 +102,7 @@ module.exports.reviewsAddOne = function(req, res){
     });//exec
 };
 
-// update one review 
+// update one review
 module.exports.reviewsUpdateOne = function(req, res){
   var hotelId = req.params.hotelId;
   var reviewId = req.params.reviewId; // PUT route in index.js
@@ -159,7 +159,56 @@ module.exports.reviewsUpdateOne = function(req, res){
 
 
 module.exports.reviewsDeleteOne = function(req, res){
+  var hotelId = req.params.hotelId;
+  var reviewId = req.params.reviewId; // PUT route in index.js
+  console.log("PUT reviewId " + reviewId + " for hotelId " + hotelId);
 
+  Hotel
+    .findById(hotelId)
+    .select("reviews")
+    .exec(function(err, hotel){
+      var thisReview;
+      var response = {status: 200, message: {} };
+
+      // changing response if something isn't right
+      if(err){
+        console.log("Error finding hotel");
+        response.status = 500;
+        response.message = err;
+      }
+      else if(!hotel){
+        console.log('hotelId not found in hotelData', id);
+        response.status = 404;
+        response.messaage = {"message": "hotel id not found " + id};
+      }
+      else{
+        thisReview = hotel.reviews.id(reviewId);
+        if(!thisReview){
+          response.status = 404;
+          response.message = {"message": "reviewId not found ", reviewId};
+        }
+      }//else
+
+      if(response.status !== 200){ // if something wasn't perfect
+        res
+          .status(response.status)
+          .json(response.message);
+      }
+      else{
+        thisReview.name = req.body.name;
+        thisReview.rating = req.body.rating;
+        thisReview.review = req.body.review;
+
+        hotel.save(function(err, hotelUpdated){
+          if(err){
+            res.status(500).json(err);
+          }
+          else{
+            res.status(204).json();
+          }
+        });//save
+      }//else
+    });//exec
 
 
 
