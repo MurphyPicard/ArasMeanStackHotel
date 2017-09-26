3dbb92df91de4cfcc39affe8c257fa629cf4f983
Ara <ayaghsizian@gmail.com>
2017-09-26 12:39:52 -0400

    my computer is having problems, will restart to troubleshoot routes

--- public/angular-app/hotel-display/hotel-display-controller.js
+++ public/angular-app/hotel-display/hotel-display-controller.js
@@ -5,7 +5,7 @@ angular.module('meanhotel').controller('HotelController', HotelController);
 function HotelController ($http, $routeParams){
   var vm = this;
   var id = $routeParams.id;
-  $http.get('/api/hotel' + id).then(function(response){
+  $http.get('/api/hotels/' + id).then(function(response){
     // console.log(response);
     vm.hotel = response.data;
   });
--- public/angular-app/hotel-list/hotel-list-controller.js
+++ public/angular-app/hotel-list/hotel-list-controller.js
@@ -4,7 +4,7 @@ angular.module('meanhotel').controller('HotelsController', HotelsController);
 function HotelsController ($http){
   var vm = this;
   vm.title = "Ara's MEEEEEEEEEEEEAAAAAAAAAAAAN Hotel App";
-  $http.get('/api/hotels?count=8').then(function(response){
+  $http.get('/api/hotels').then(function(response){
     // console.log(response);
     vm.hotels = response.data;
   })
